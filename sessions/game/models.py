ce9e8ba9126c73ce8160857cbbeb30ff3414ec03
Ilya Erlingas <Aligruit@gmail.com>
2020-08-23 03:52:23 +0300

    updated models

--- sessions/game/models.py
+++ sessions/game/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class Player(models.Model):
-    player_id = models.CharField(primary_key=True)
+    player_id = models.CharField(max_length=10, primary_key=True)
 
 
 class Game(models.Model):
-    game_id = models.CharField(primary_key=True)
+    game_id = models.CharField(max_length=10, primary_key=True)
+    number = models.CharField(max_length=1, )
 
 
 class PlayerGameInfo(models.Model):
